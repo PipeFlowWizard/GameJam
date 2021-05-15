715ab1ba56c1a5e24ff49f141bb73e9397da6e75
Nebularswamp <40129612+Nebularswamp@users.noreply.github.com>
2021-05-15 01:00:57 -0400

    simple Menu Added

---
+++ Project/Assets/Scripts/Menu.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Menu : MonoBehaviour
+{
+    
+    public void ExitGame()
+    {
+        Application.Quit();
+    }
+
+    // Update is called once per frame
+    public void StartGame()
+    {
+        //starts startingscene
+    }
+}
