d5f1722b190d8f0c62d2a926493afed07f7ea215
PipeFlowWizard <elijahmon96@gmail.com>
2021-05-15 00:33:59 -0400

    Update Appendage.cs

--- Project/Assets/Scripts/Appendage.cs
+++ Project/Assets/Scripts/Appendage.cs
@@ -1,10 +1,26 @@
-﻿using System.Collections;
-using System.Collections.Generic;
+﻿
 using UnityEngine;
 
 public class Appendage : MonoBehaviour
 {
     public bool isSocketed = false;
     public Socket socket;
+    public string Side;
+    public string Type;
+    public AppendageAsset asset;
+    public GameObject appendage;
 
+    private void Start()
+    {
+        this.Side = asset.Side;
+        this.tag = asset.Type;
+        if(asset != null)
+            ResetAppendage();
+    }
+
+    public void ResetAppendage()
+    {
+        appendage = Instantiate(asset.AppendagePrefab, transform);
+    }
 }
+
