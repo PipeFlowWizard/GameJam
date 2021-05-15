d570305db74f7f6c01588bb5a013410be1367254
Benjamin Nielsen <56326234+ben-baqa@users.noreply.github.com>
2021-05-15 15:34:00 -0600

    Spawner prefab

---
+++ Project/Assets/Scripts/Spawner.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawner : MonoBehaviour
+{
+    public GameObject[] blobPrefabs, limbPrefabs;
+    public Vector2 spawnBounds;
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.DrawWireCube(Vector3.zero, spawnBounds);
+    }
+
+    public void SpawnBlob()
+    {
+        Vector2 spawnPos = GenerateSpawnPosition();
+        Instantiate(blobPrefabs[Random.Range(0, blobPrefabs.Length)], spawnPos, Quaternion.identity);
+    }
+
+    public void SpawnBlob(int n)
+    {
+        Vector2 spawnPos = GenerateSpawnPosition();
+        Blob blob = Instantiate(blobPrefabs[Random.Range(0, blobPrefabs.Length)], spawnPos,
+            Quaternion.identity).GetComponent<Blob>();
+
+        while( n > 0)
+        {
+            blob.Attach(Instantiate(limbPrefabs[Random.Range(0, limbPrefabs.Length)]).GetComponent<Appendage>());
+            n--;
+        }
+    }
+
+    public void SpawnAppendage()
+    {
+        Vector2 spawnPos = GenerateSpawnPosition();
+        Instantiate(limbPrefabs[Random.Range(0, limbPrefabs.Length)], spawnPos, Quaternion.identity);
+    }
+
+    private Vector2 GenerateSpawnPosition()
+    {
+        Vector2 spawnPos = Vector2.zero;
+        spawnPos.x = Random.Range(-spawnBounds.x, spawnBounds.x);
+        spawnPos.y = Random.Range(-spawnBounds.y, spawnBounds.y);
+        return spawnPos;
+    }
+}
