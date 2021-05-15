611e050b024e18b18515bc75b46c75f46242ac02
Benjamin Nielsen <56326234+ben-baqa@users.noreply.github.com>
2021-05-14 21:08:44 -0600

    Basic Arrow Stuff

---
+++ Project/Assets/Scripts/ArrowBehavior.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ArrowBehavior : MonoBehaviour
+{
+    public float speed;
+
+    private Rigidbody2D rb;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        rb.velocity = transform.up * speed;
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        rb.constraints = RigidbodyConstraints2D.FreezeAll;
+        transform.parent = collision.transform;
+        // selector.Select(collision.GetComponent<Shootable>());
+        Destroy(rb);
+        Destroy(GetComponent<BoxCollider2D>());
+        Destroy(this);
+    }
+}
---
+++ Project/Assets/Scripts/ArrowDispenser.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ArrowDispenser : MonoBehaviour
+{
+    public GameObject arrowPrefab;
+
+    private ControlKey input;
+    // Start is called before the first frame update
+    void Start()
+    {
+        input = GetComponent<ControlKey>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (input["fire"])
+            Instantiate(arrowPrefab, transform.position + transform.up,
+                transform.rotation);
+    }
+}
