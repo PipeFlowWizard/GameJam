8af287b58db86a40b680b71ffa8fbb5cafd57b58
Benjamin Nielsen <56326234+ben-baqa@users.noreply.github.com>
2021-05-14 20:24:18 -0600

    Oversimplified implementation of Socket script

---
+++ Project/Assets/Scripts/Socket.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Socket : MonoBehaviour
+{
+    public void Attach(Socket socket)
+    {
+        transform.parent = socket.transform;
+        transform.localPosition = Vector2.zero;
+    }
+}
