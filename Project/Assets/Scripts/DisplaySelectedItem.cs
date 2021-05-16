7e21f2d69db0ae5c685e0db7c218aaa39fb8c056
Benjamin Nielsen <56326234+ben-baqa@users.noreply.github.com>
2021-05-16 11:39:11 -0600

    Selected Item Display

---
+++ Project/Assets/Scripts/DisplaySelectedItem.cs
@@ -0,0 +1,80 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class DisplaySelectedItem : MonoBehaviour
+{
+    public Sprite blob, leftArm, rightArm, leftLeg, rightLeg, empty;
+    public TextMeshProUGUI text;
+
+    private SpriteRenderer rend;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rend = GetComponent<SpriteRenderer>();
+        DisplayNoSelection();
+    }
+
+    public void UpdateDisplay(GameObject obj)
+    {
+        if (!obj)
+        {
+            DisplayNoSelection();
+            return;
+        }
+
+        if (obj.CompareTag("Blob"))
+        {
+            rend.sprite = blob;
+            SetText("Cutie");
+            return;
+        }
+
+        Appendage ap = obj.GetComponent<Appendage>();
+        if (!obj.CompareTag("Appendage"))
+        {
+            DisplayNoSelection();
+            return;
+        }
+
+        if(ap.Type == "Arm")
+        {
+            if(ap.Side == "Left")
+            {
+                rend.sprite = leftArm;
+                SetText("Left Arm");
+            }
+            else
+            {
+                rend.sprite = rightArm;
+                SetText("Right Arm");
+            }
+        }
+        else
+        {
+            if (ap.Side == "Left")
+            {
+                rend.sprite = leftLeg;
+                SetText("Left Leg");
+            }
+            else
+            {
+                rend.sprite = rightLeg;
+                SetText("Right Leg");
+            }
+        }
+    }
+
+    private void DisplayNoSelection()
+    {
+        rend.sprite = empty;
+        SetText("None");
+    }
+
+    private void SetText(string s)
+    {
+        if (text)
+            text.text = s;
+    }
+}
