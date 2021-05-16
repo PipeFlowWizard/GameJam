644abef2fe88c56f54b2dcbd9c8a554bf786743f
Benjamin Nielsen <56326234+ben-baqa@users.noreply.github.com>
2021-05-15 22:13:36 -0600

    Random Backgrounds + Wiggly UI

---
+++ Project/Assets/Scripts/Breathe.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Breathe : MonoBehaviour
+{
+    public Vector2 amplitude;
+    public Vector2 frequency;
+
+    private Vector2 scale;
+    // Start is called before the first frame update
+    void Start()
+    {
+        scale = transform.localScale;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (transform != transform.root)
+            Destroy(this);
+        transform.localScale = scale + new Vector2(
+            amplitude.x * Mathf.Sin(Time.time * frequency.x), amplitude.y * Mathf.Cos(Time.time * frequency.y));
+    }
+}
---
+++ Project/Assets/Scripts/SelectSpriteFromPool.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SelectSpriteFromPool : MonoBehaviour
+{
+    public Sprite[] spritePool;
+    // Start is called before the first frame update
+    void Start()
+    {
+        GetComponent<SpriteRenderer>().sprite = spritePool[Random.Range(0, spritePool.Length)];
+    }
+}
